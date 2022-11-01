7024817103e3ca84b4ea89b2c8f75edbdbfc3b12
AugustoMagalhaes <augustommg@gmail.com>
2022-11-01 16:43:26 -0300

    adiciona dependencias do kafka e kafka-streams ao projeto

--- project.clj
+++ project.clj
@@ -1,9 +1,19 @@
 (defproject projeto-final "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Projeto final de integração Clojure, Kafka e Cassandra"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[com.fzakaria/slf4j-timbre "0.3.17"]
+                 [org.apache.kafka/kafka-streams "3.2.1"]
+                 [org.clojure/clojure "1.10.0"]
+                 [org.clojure/data.json "2.4.0"]
+                 [org.clojure/tools.logging "0.4.0"]
+                 [org.apache.kafka/kafka-clients "3.0.0"]
+                 [org.apache.kafka/kafka_2.12 "3.0.0"]
+                 [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
+                                                    javax.jms/jms
+                                                    com.sun.jmdk/jmxtools
+                                                    com.sun.jmx/jmxri]]]
   :main ^:skip-aot projeto-final.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
