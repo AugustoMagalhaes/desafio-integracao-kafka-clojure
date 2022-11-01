9287bc251148f7cf27f12bcb296c7f0e528edf0e
AugustoMagalhaes <augustommg@gmail.com>
2022-11-01 17:21:14 -0300

    adiciona importaçoes e require ao core.clj

--- src/projeto_final/core.clj
+++ src/projeto_final/core.clj
@@ -1,7 +1,21 @@
 (ns projeto-final.core
-  (:gen-class))
+  (:gen-class)
+  (:require
+   [clojure.tools.logging :as log]
+   [taoensso.timbre :as logt]
+   [clojure.data.json :as json])
+  (:import
+   [java.util Properties]
+   [org.apache.kafka.common.serialization Serdes Serde Serializer Deserializer StringSerializer StringDeserializer]
+   [org.apache.kafka.streams KafkaStreams StreamsConfig Topology]
+   [org.apache.kafka.streams.processor Processor ProcessorSupplier To]
+   [org.apache.kafka.clients.admin AdminClientConfig NewTopic KafkaAdminClient]
+   org.apache.kafka.clients.consumer.KafkaConsumer
+   [org.apache.kafka.clients.producer KafkaProducer ProducerRecord]
+   (org.apache.kafka.common TopicPartition)
+   (java.time Duration)))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println Properties Serdes Serde Serializer Deserializer StringSerializer StringDeserializer KafkaStreams StreamsConfig Topology Processor ProcessorSupplier To .log .logt .json AdminClientConfig NewTopic KafkaAdminClient KafkaConsumer KafkaProducer ProducerRecord TopicPartition Duration))
