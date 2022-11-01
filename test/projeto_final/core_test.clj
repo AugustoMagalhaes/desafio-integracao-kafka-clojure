4a93f48b4608f8ae24ba5c99465dc1cb5b92abd2
AugustoMagalhaes <augustommg@gmail.com>
2022-11-01 16:09:11 -0300

    inicia projeto final com lein

---
+++ test/projeto_final/core_test.clj
@@ -0,0 +1,7 @@
+(ns projeto-final.core-test
+  (:require [clojure.test :refer :all]
+            [projeto-final.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
