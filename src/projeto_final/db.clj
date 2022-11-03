3a579affb0f980436cf985678105241a19b886dd
Julio Marodin <juliocesar_cocacola@hotmail.com>
2022-11-03 18:56:47 -0300

    criadas queries de consulta e criacao

--- src/projeto_final/db.clj
+++ src/projeto_final/db.clj
@@ -6,3 +6,25 @@
                             :contact-points ["host.docker.internal:7000"]}))
 
 (alia/execute session "CREATE KEYSPACE testegrupo WITH replication = {:'class' 'SimpleStrategy', 'replication_factor': 3};")
+
+(def prepara (alia/prepare session "INSERT INTO users
+                         (user_name, first_name, last_name)
+                         VALUES(?, ?, ?);"))
+
+(defn cria [nome primeiro ultimo]
+  (alia/execute session prepara {:values [nome, primeiro, ultimo]}))
+
+(defn le [nome] (alia/execute session "select * from users where user_name=?" {:values nome}))
+
+(def prepara-le (alia/prepare session "select * from projetoteste.users where user_name= :nome limit :lmt;"))
+
+(defn le [nome] (alia/execute session prepara-le {:values {:nome nome :lmt (int 1)}}))
+
+;; (defn -main
+;;   "I don't do a whole lot ... yet."
+;;   [& args]
+;;   (cria "" "" "")
+;;   (println "começooooooooooo")
+;;   (let [teste (le "kung fu panda")]
+;;     (println teste))
+;;   (println "acaboooooooo"))
